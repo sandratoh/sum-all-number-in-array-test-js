68d2608157aec8e98f8eead6f600d85136f79385
Sandra To <sandra.to.e@gmail.com>
2021-02-16 05:04:45 +0000

    Base case codes added

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-function sumItems(array) {
+const sumItems = array => {
   // Sum all the numbers in the array
-}
+  // base case: if only one layer of array
+  let result = array.reduce((sum, i) => sum + i);
+  return result;
+  
+  // let sum;
+  // array.forEach((num) => {
+  //   sum += num;
+  //   // return num;
+  // });
+  // // recursive case: if nested array
+  // // if (Array.isArray(item)) {
+
+  // // }
+};
 
 module.exports = sumItems;
+
+// Logic:
+// sumItems[0, 1] = 1
+// sumItems[0, [1]] = 0 + [1] = 0 + 1
+// sumItems[0, [[[1]]]] = 0 + [[1]] = 0 + [1] = 0 + 1
+
+// Pattern:
+// sumItems[0, 1] = 1
+// sumItems[0, [1]] = sumItems[0, 1] + sumItems[1]
+// sumItems[0, [[1]]] = sumItms[0, 1] + sumItems[[1]]
+
+// Recursion Implementation
+// sumItems[arr, [nestedArr]] = sumItems[arr] + sumItems[nestedArr]
